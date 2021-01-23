1159f3c2c74041336573a9b127166223d1111b03
Krupin.SA <krupin.sergrey@gmail.com>
2021-01-23 22:25:27 +0500

    Add function get_file_extension

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import os
 
 img_path = 'images'
 
-def download_save(url, full_path):
+def get_file_extension(url: str) -> str:
+    parts_path = url.split('/')
+    file_name = parts_path[-1]
+    return file_name.split('.')[-1]
+
+def download_save(url: str, full_path: str) -> None:
     response = requests.get(url)
     response.raise_for_status()
     with open(full_path, 'wb') as photo:
         photo.write(response.content)
 
-def fetch_spacex_last_launch():
+def fetch_spacex_last_launch() -> None:
     spacex_url = "https://api.spacexdata.com/v4/launches/latest"
 
     response = requests.get(spacex_url)
@@ -29,10 +34,10 @@ def main():
     fetch_spacex_last_launch()
     hubble_url = 'http://hubblesite.org/api/v3/image/3811'
     response = requests.get(hubble_url)
-    print(response.text)
     photos = response.json()['image_files']
     for cur_photo in photos:
         print(cur_photo['file_url'])
+        print(get_file_extension(cur_photo['file_url']))
 
 if __name__=="__main__":
     main()
